a4b9cffd6239b40e02115ba40bfdf7967c1ef27a
karina <karina>
2017-02-06 01:05:16 -0600

    movement of player added

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Implements control of the player
+/// </summary>
+public class PlayerController : MonoBehaviour
+{
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    /// <summary>
+    ///  Called once per grade
+    /// </summary>
+    internal void Update()
+    {
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            
+            transform.position += transform.up.normalized * 2 * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+
+            transform.position -= transform.up.normalized * 2 * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+
+            transform.position -= transform.right.normalized * 2 * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += transform.right.normalized * 2 * Time.deltaTime;
+        }
+
+    }
+
+
+}
