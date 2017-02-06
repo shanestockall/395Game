0802e03740cd42dc6e124758aecf3944215ec746
Shane Stockall <shanestockall@dhcp-10-105-182-251.wireless.northwestern.private>
2017-02-06 12:32:32 -0600

    fix bad commits because github lol

---
+++ Assets/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public MapBuilder boardScript; 
+	private int level = 1; 
+
+	// Use this for initialization
+	void Awake () {
+		boardScript = GetComponent<MapBuilder> (); 
+		InitGame (); 
+		
+	}
+
+	void InitGame(){
+		boardScript.SetupScene (level);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
