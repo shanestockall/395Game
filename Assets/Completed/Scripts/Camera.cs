72e6025514ff5c0d921768e5d76af9c769b2fa89
Shane Stockall <shanestockall@dhcp-10-105-153-217.wireless.northwestern.private>
2017-02-15 18:33:50 -0600

    new stuff too

---
+++ Assets/Completed/Scripts/Camera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour {
+
+	public Transform player = null;
+	public float cameraHeight = 20.0f;
+	private Transform cam = null;
+
+
+	public void Start()
+	{
+		cam = transform;
+	}
+	public void Update()
+	{
+		Vector3 pos = player.position;
+		pos.y = cameraHeight;
+		cam.position = pos;
+	}
+}
